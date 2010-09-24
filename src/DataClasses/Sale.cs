95ec4697250583a4d5c95df5434890497bbe062d
sidneyfilho <sidney.filho@vivina.com.br>
2010-09-24 15:44:43 +0000

    Criado a classe parcial de Sale para não expor o contas a pagar nem parcelas

---
+++ src/DataClasses/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vivina.Erp.DataClasses
+{
+    public partial class Sale
+    {
+        public FinancierOperation FinancierOperation
+        {
+            get
+            {
+                return this.Invoice.Parcels.First().FinancierOperation;
+            }
+        }
+    }
+}
