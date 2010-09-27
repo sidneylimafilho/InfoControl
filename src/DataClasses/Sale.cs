357d313598f048374370d67b64db4d3ceef652ae
sidneyfilho <sidney.filho@vivina.com.br>
2010-09-27 01:07:58 +0000

    Alteração da ordem de colunas e adição de relacionamentos

--- src/DataClasses/Sale.cs
+++ src/DataClasses/Sale.cs
@@ -7,12 +7,19 @@ namespace Vivina.Erp.DataClasses
 {
     public partial class Sale
     {
+        public Sale(int saleStatusId)
+        {
+            SaleStatusId = saleStatusId;
+        }
+
+        public decimal Total
+        {
+            get { return SaleItems.Sum(si => si.Quantity * si.UnitPrice) + FreightValue.Value; }
+        }
+
         public FinancierOperation FinancierOperation
         {
-            get
-            {
-                return this.Invoice.Parcels.First().FinancierOperation;
-            }
+            get { return this.Invoice.Parcels.First().FinancierOperation; }
         }
     }
 }
