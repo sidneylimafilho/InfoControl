060d87a71456a08f1aa7cb23150c591cf2358bc4
sidneyfilho <sidney.filho@vivina.com.br>
2010-10-22 20:25:13 +0000

    Removida a função LoadAll que não é mais utilizada

--- src/Framework/ValueTypeExtensions.cs
+++ src/Framework/ValueTypeExtensions.cs
@@ -273,17 +273,17 @@ namespace InfoControl
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="original"></param>
-        public static T LoadAll<T>(this T original) where T : class
-        {
-            if (original != null)
-            {
-                PropertyInfo[] currentProps = original.GetType().GetProperties();
-                foreach (PropertyInfo prop in currentProps)
-                    if (prop.GetCustomAttributes(typeof(AssociationAttribute), true).Length == 1)
-                        prop.GetValue(original, null);
-            }
-            return original;
-        }
+        //public static T LoadAll<T>(this T original) where T : class
+        //{
+        //    if (original != null)
+        //    {
+        //        PropertyInfo[] currentProps = original.GetType().GetProperties();
+        //        foreach (PropertyInfo prop in currentProps)
+        //            if (prop.GetCustomAttributes(typeof(AssociationAttribute), true).Length == 1)
+        //                prop.GetValue(original, null);
+        //    }
+        //    return original;
+        //}
 
         /// <summary>
         /// 
