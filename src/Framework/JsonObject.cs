126003aa82d7a956cf138ae98f2cfc9c9c63cb98
sidneyfilho <sidney.filho@vivina.com.br>
2010-10-25 19:34:04 +0000

    Criado uma classe em C# que representará um objeto JSON, por isso foi usado um Hastable para receber quaisquer tipo de dados.

---
+++ src/Framework/JsonObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace InfoControl
+{
+    /// <summary>
+    /// Represents a object that can be identificable
+    /// </summary>
+    [DataContract]
+    public class JsonObject : Hashtable
+    {
+        new public JsonObject this[object key]
+        {
+            get
+            {
+                if (!ContainsKey(key))
+                    throw new KeyNotFoundException(key.ToString());
+
+                return base[key] as JsonObject;
+            }
+            set
+            {
+                base[key] = value;
+            }
+        }
+    }
+}
