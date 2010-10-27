cd4d74a30188eae4839a04fb86c45b468852f505
sidneyfilho <sidney.filho@vivina.com.br>
2010-10-27 12:02:05 +0000

    Adicionada uma propriedade que retorna a url de acesso a página

--- src/DataClasses/WebSites/WebPage.partial.cs
+++ src/DataClasses/WebSites/WebPage.partial.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Web;
+
+using InfoControl;
 
 
 namespace Vivina.Erp.DataClasses
 {
     public partial class WebPage : INotifyPropertyChanging, INotifyPropertyChanged
     {
+        public string Url
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(RedirectUrl))
+                    return RedirectUrl;
 
-        
+                string url = "";
 
+                var webPage = this;                
+                while ((webPage = webPage.WebPage1) != null)
+                    url = webPage.Name.Trim().RemoveSpecialChars() + "/" + url;
 
+                return "~/site/" + url + Name.RemoveSpecialChars() + "," + WebPageId + ".aspx";                
+            }
+        }               
     }
 }
