b916846eed0441eb8f5095de6dbaae8f2e040add
sidneyfilho <sidney.filho@vivina.com.br>
2010-11-11 19:50:23 +0000

    Criação do Accounting Service e conversão de controller

---
+++ src/WebUI/InfoControl/Accounting/AccountingService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using InfoControl;
+using InfoControl.Web;
+using Vivina.Erp.BusinessRules;
+using Vivina.Erp.BusinessRules.Reports;
+using Vivina.Erp.SystemFramework;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections;
+using Vivina.Erp.DataClasses;
+using System.Web.Mvc;
+using InfoControl.Web.Services;
+using Vivina.Erp.BusinessRules.Services;
+
+namespace Vivina.Erp.WebUI.InfoControl.Accounting
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class AccountingService : DataServiceBase
+    {
+        [OperationContract, JavaScriptSerializer]
+        public ActionResult GetCashFlowByYear(Hashtable Params, Hashtable FormData)
+        {
+
+            using (var manager = new AccountManager(null))
+                return ClientResponse(() => manager.GetCashFlowByYear(Company.CompanyId,
+                                                                      Convert.ToInt32(Params["accountPlanId"]),
+                                                                      Convert.ToInt32(Params["year"])));
+        }
+    }
+}
+
