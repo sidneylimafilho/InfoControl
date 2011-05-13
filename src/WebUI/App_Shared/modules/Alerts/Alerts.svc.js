9f4607e728e02e7cdb0ccc41278058f6d56a6f1e
Sidney <sidney.filho@vivina.com.br>
2011-05-13 00:37:43 -0300

    Alterado a rotina de Alerts para só executar quando o jQuery já estiver carregado

--- src/WebUI/App_Shared/modules/Alerts/Alerts.svc.js
+++ src/WebUI/App_Shared/modules/Alerts/Alerts.svc.js
@@ -1,35 +1,35 @@
-﻿window.InitAlertMonitor = function (Alerts) {
-    if (!top.document.originalTitle)
-        top.document.originalTitle = top.document.title;
+﻿(function($) {
+    window.InitAlertMonitor = function(Alerts) {
+        if (!top.document.originalTitle)
+            top.document.originalTitle = top.document.title;
 
-    Alerts = Alerts || {};
+        Alerts = Alerts || {};
 
-    if (!Alerts.Count)
-        Alerts.Count = 0;
-    if (!Alerts.LastAlertId)
-        Alerts.LastAlertId = 0;
+        if (!Alerts.Count)
+            Alerts.Count = 0;
+        if (!Alerts.LastAlertId)
+            Alerts.LastAlertId = 0;
 
-    Alerts.SetTitle = function (count) {
-        top.document.title = "Você tem " + count + " mensagens!";
-        top.focus();
-        if (count == 0)
-            Alerts.RestoreTitle();
-    }
+        Alerts.SetTitle = function(count) {
+            top.document.title = "Você tem " + count + " mensagens!";
+            top.focus();
+            if (count == 0)
+                Alerts.RestoreTitle();
+        }
 
-    Alerts.RestoreTitle = function () {
-        top.document.title = top.document.originalTitle;
-    }
+        Alerts.RestoreTitle = function() {
+            top.document.title = top.document.originalTitle;
+        }
 
-    Alerts.timerId = setTimeout(function () {
-        Alerts.GetAlerts(
-            function (result, response, context) {
+        Alerts.timerId = setTimeout(function() {
+            Alerts.GetAlerts(function(result, response, context) {
                 result = eval(result);
 
                 for (var i = 0; i < result.length; i++) {
                     if (Alerts.LastAlertId < result[i].AlertId) {
-                        $.jGrowl(result[i].Description, {
+                        top.$.jGrowl(result[i].Description, {
                             id: result[i].AlertId,
-                            beforeClose: function (e, m, o) {
+                            beforeClose: function(e, m, o) {
                                 Alerts.Delete(o.id);
                                 Alerts.SetTitle(--Alerts.Count);
                             }
@@ -42,5 +42,6 @@
                 InitAlertMonitor();
             }
         )
-    }, 3000);
-}
+        }, 3000);
+    }
+})(jQuery);
